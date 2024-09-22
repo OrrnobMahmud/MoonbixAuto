607cd48e28991e4910c40b39ea068cf3efbe691c
Orrnob Mahmud <53536803+OrrnobMahmud@users.noreply.github.com>
2024-09-22 06:01:13 +0600

    Update test.py

--- test.py
+++ test.py
@@ -393,4 +393,5 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print(f"{Fore.RED}\nProcess interrupted by user. Exiting...{
+        print(f"{Fore.RED}\nProcess interrupted by user. Exiting...{Style.RESET_ALL}")
+        logging.info("Process interrupted by user. Exiting...")
